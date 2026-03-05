c1f3a17621d52725c3fcb56aa06b7bfdadf42ec2
Manin611 <samuelteby@gmail.com>
2026-03-04 20:16:45 -0500

    Ultimos5

--- ejercicios/ejercicio_06.py
+++ ejercicios/ejercicio_06.py
@@ -2,17 +2,23 @@
 
 # Ejercicio 1: Crea una lista con los nombres de 3 colores e imprímela.
 # Escribe tu código debajo de esta línea:
-
+colores = ["naranja", "negro", "dorado"]
+print(colores)
 
 # Ejercicio 2: Crea una tupla con 2 coordenadas (ej. x, y) e imprímela.
 # (Nota: las tuplas no se pueden modificar una vez creadas)
 # Escribe tu código debajo de esta línea:
-
+coordenadas = (35, 139)
+print(coordenadas)
 
 # Ejercicio 3: Crea un conjunto (set) con 3 de tus frutas favoritas e imprímelo.
 # Escribe tu código debajo de esta línea:
+frutas = {"mango", "fresa", "Sandia"}
+print(frutas)
 
 
 # Ejercicio 4: Crea un diccionario con la información de un libro ("titulo" y "autor") e imprímelo.
 # Escribe tu código debajo de esta línea:
+libro = {"titulo": "Pandora Hearts", "autor": "Jun Mochizuki"}
+print(libro)
 
--- ejercicios/ejercicio_07.py
+++ ejercicios/ejercicio_07.py
@@ -3,16 +3,22 @@
 # Ejercicio 1: Pide al usuario que ingrese su nombre usando input() y luego imprime un saludo 
 # personalizado que incluya su nombre.
 # Escribe tu código debajo de esta línea:
-
+nombre = input("Ingresa tu nombre: ")
+print("Hola,", nombre + "! Bienvenido a mi codigo.")
 
 # Ejercicio 2: Pide al usuario que ingrese su edad como texto (input siempre devuelve texto).
 # Convierte esa edad a un número entero (casting) y luego calcula y muestra cuántos años tendrá 
 # en el año 2050 (puedes asumir que el año actual es 2024 para el cálculo, o restarle su año de nacimiento).
 # Simplificado: asume que sumarle 26 años le da su edad en 2050.
 # Escribe tu código debajo de esta línea:
-
+edad = int(input("Ingresa tu edad: "))
+edad_2050 = edad + 26
+print("En el año 2050 tendrás", edad_2050, "años")
 
 # Ejercicio 3: Pide al usuario que ingrese el precio de un producto. Convierte el valor ingresado 
 # a un número decimal (float). Luego, calcula y muestra el precio con un 20% de descuento.
 # Escribe tu código debajo de esta línea:
-
+precio = float(input("Ingresa el precio del producto: "))
+descuento = precio * 0.20
+precio_final = precio - descuento
+print("El precio con descuento es:", precio_final)
